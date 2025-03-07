f6e6ac977571850e5a898cbb7ba6bf73d6941aa1
Joot <jyotsananair2006@gmail.com>
2025-03-07 13:41:58 +1100

    Create script.js

---
+++ script.js
@@ -0,0 +1,22 @@
+const inputBox = document.getElementById("input-box");
+const listContainer = document.getElementById("list-container");
+
+function addTask() {
+    const task = inputBox.value.trim();
+    if (!task) {
+      alert("Please write down a task");
+      return;
+    }
+  
+    li.innerHTML = `
+      <label>
+        <input type="checkbox">
+        <span>${task}</span>
+      </label>
+      <span class="edit-btn">Edit</span>
+      <span class="delete-btn">Delete</span>
+      `;
+  
+    listContainer.appendChild(li);
+    inputBox.value = "";
+  }
